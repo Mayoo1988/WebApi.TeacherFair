5c66e10079edb79938cc1efde243ffda9bed7c2f
Mayur <Mayur@DELL>
2017-06-26 21:15:11 +0530

    Commit Changes

---
+++ WebApi.TeacherFair/MyAuthorizationServiceProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+
+namespace WebApi.TeacherFair
+{
+    public class MyAuthorizationServiceProvider:OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        //Validate client authentication and create token
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if(context.UserName=="admin"&& context.Password=="admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Rohan Mane"));
+                context.Validated(identity);
+            }
+
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Mayur Mane"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid Credentials");
+                return;
+            }
+
+        }
+    }
+}
---
+++ WebApi.TeacherFair/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApi.TeacherFair.Startup))]
+
+namespace WebApi.TeacherFair
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Enabling cross origin request
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            var MyProvider = new MyAuthorizationServiceProvider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                Provider = MyProvider,
+        };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
